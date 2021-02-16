d4e36dbbe67ba68a602cbda08278df572e6bd4e1
Michael Shin <michaelshin@Admins-MacBook-Pro.local>
2021-02-15 22:58:48 -0800

    edited part2

--- OOP/Bat.java
+++ OOP/Bat.java
@@ -1,6 +1,6 @@
 public class Bat extends Mammal{
 	public Bat() {
-		setEnergy(300);
+		setEnergy(100);
 	}
 	public void eatHumans() {
 		setEnergy(getEnergy() + 15);
@@ -8,13 +8,13 @@ public class Bat extends Mammal{
 		displayEnergy();
 	}
 	public void attackTown() {
-		setEnergy(getEnergy() - 80);
-		System.out.println("The Bat is flying, energy decreased by 80!");
+		setEnergy(getEnergy() - 5);
+		System.out.println("The Bat is flying, energy decreased by 5!");
 		displayEnergy();
 	}
     public void fly() {
-		setEnergy(getEnergy() - 35);
-		System.out.println("flying bat watch out! its energy decreased by 35!");
+		setEnergy(getEnergy() - 10);
+		System.out.println("flying bat watch out! its energy decreased by 10!");
 		displayEnergy();
 	}
 
